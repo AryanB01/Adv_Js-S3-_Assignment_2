2195d70f78275c40217b38f00145178d77418185
AryanB01 <aryanbhattarai000@gmail.com>
2023-10-05 18:47:10 -0400

    all done some changes need to be made as a final check

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,6 +3,7 @@
   let LoadHeader = () => {
     $.get("./views/shared/header.html", (htmlData) => {
       $("header").html(htmlData);
+      LoadContent(); // this is for loading home as I want home to show instead of index at first.
 
       //after navbar loaded attaching event handlers to each link
       $("li>a").on("click", (event) => {
@@ -16,12 +17,14 @@
       //})
     });
   };
+
   // for loading footer
   let LoadFooter = () => {
     $.get("./views/shared/footer.html", (htmlData) => {
       $("footer").html(htmlData);
     });
   };
+
   //   for main content
   let LoadContent = () => {
     let activePage = document.title;
@@ -30,39 +33,52 @@
       // adding current page to top of history
       history.pushState({}, "", `/${document.title}`);
     });
+
+    //$("#btnAboutme").on("click", (event) => { : this didn't work as i believe it was because of the home page dynamically getting added to the first index page.
+    $(document).on("click", ".btnAboutme", (event) => {
+      // console.log("test");
+      event.preventDefault();
+      $.get(`./views/aboutme.html`, (htmlData) => {
+        $("main").html(htmlData);
+        history.pushState({}, "", `/${document.title}`);
+      });
+    });
+
+    $(document).on("click", ".contactMe", (event) => {
+      event.preventDefault();
+      $.get(`./views/contactme.html`, (htmlData) => {
+        $("main").html(htmlData);
+        history.pushState({}, "", `/${document.title}`);
+      });
+    });
+
+    // redirecting user to aboutme page when the contace form is submitted
+    $(document).on("submit", "#formContact", function () {
+      event.preventDefault();
+      $.get(`./views/aboutme.html`, (htmlData) => {
+        $("main").html(htmlData);
+        history.pushState({}, "", `${document.title}`);
+      });
+    });
   };
 
   let Start = () => {
-    // setting the landing page to home.html
-    if ((document.title = "Portfolio")) {
+    LoadHeader();
+    LoadFooter();
+
+    // setting the landing page to home
+    let activePage = document.title;
+    if (activePage == "Portfolio") {
+      // console.log("test here");
+      document.title = "home";
       $.get(`./views/home.html`, (htmlData) => {
         $("main").html(htmlData);
+
         // adding home to top of history
         history.pushState({}, "", `/${document.title}`);
       });
     }
-
-    LoadHeader();
-    LoadFooter();
   };
-  // let button = document.querySelector(".btn");
-  
-  // buttonClicked = () => {
-  //   $.get(`./views/aboutme.html`, (htmlData) => {
-  //     $("main").html(htmlData);
-  //     history.pushState({}, "", `/${document.title}`);
-  //   });
-  // };
-  // button.addEventListener('click', buttonClicked())
 
   window.addEventListener("load", Start);
 })();
-
-// buttonClick = () => {
-//   LoadContent();
-//   window.location.href = "./views/aboutme.html";
-// };
-// let button = document.querySelector(".btn");
-// button.addEventListener("click", buttonClick);
-
-// let displayContent = () => {};
---
+++ readme.md
@@ -0,0 +1,4 @@
+The purpose of this site is to dynamically direct the user to the different section of the page (Home, About Me, Skills, Projects, Contact Me) without having to reload the whole page. Immediately Invoked Function Expression (IIFE) is used to prevent the global scope of the functions. 
+Firstly the windows load the Start function which has the shared header and footer for the page. in the header file, there are links to different pages of the sites which is inserted in the landing page(index) dynamically. and the footer contains the copyright information.
+Load Content function loads the nav pages dynamically, without having to reload header and footer. It also directs user to about me and contact me when clicked in the body of main page. Each time the Load content directs the user to other nav links it dynamically stacks the history of the previous page. 
+
