c0ba1b79ee9caf300f96646d0a241b8ffa536c24
AryanB01 <aryanbhattarai000@gmail.com>
2023-11-07 13:01:14 -0500

    updating the porfolio site and fixing issues related to reloading and rendering

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,82 +1,122 @@
-(function () {
-  // for loading header
-  let LoadHeader = () => {
-    $.get(`./views/shared/header.html`, (htmlData) => {
-      $("header").html(htmlData);
-      //after navbar loaded attaching event handlers to each link
-      $("li>a").on("click", (event) => {
-        // prevents default behaviour
-        event.preventDefault();
+(function () 
+{
 
-        // set the page title to the id attribute of the selected a element
-        document.title = $(event.currentTarget).prop("id");
-        LoadContent();
-      });
-      //})
-    });
-  };
+  		// for loading header
+    let LoadHeader = () => {
+            $.get(`./views/shared/header.html`, (htmlData) => {
+                $("header").html(htmlData);
+                //after navbar loaded attaching event handlers to each link
+                $("li>a").on("click", (event) => {
+                  // prevents default behaviour
+                  event.preventDefault();
 
-  // for loading footer
-  let LoadFooter = () => {
-    $.get(`./views/shared/footer.html`, (htmlData) => {
-      $("footer").html(htmlData);
-    });
-  };
+                  // set the page title to the id attribute of the selected a element
+                  document.title = $(event.currentTarget).prop("id");
+                  LoadContent();
+                });
+                //})
+              });
+            };
 
-  //   for main content
-  let LoadContent = () => {
-    let activePage = document.title;
-    $.get(`./views/${activePage}.html`, (htmlData) => {
-      $("main").html(htmlData);
-      // adding current page to top of history
-      history.pushState({}, "", `/${document.title}`);
-    });
+        // for loading footer
+    let LoadFooter = () => {
+			$.get(`./views/shared/footer.html`, (htmlData) => {
+				$("footer").html(htmlData);
+				});
+			};
+        //-----------------//
 
-    //$("#btnAboutme").on("click", (event) => { : this didn't work as i believe it was because of the home page dynamically getting added to the first index page.
-    $(document).on("click", ".btnAboutme", (event) => {
-      // console.log("test");
-      event.preventDefault();
-      $.get(`./views/aboutme.html`, (htmlData) => {
-        $("main").html(htmlData);
-        history.pushState({}, "", `/${document.title}`);
-      });
-    });
+        //   for main content
+    let LoadContent = () => {
+			let activePage = document.title;
+			$.get(`./views/${activePage}.html`, (htmlData) => {
+				$("main").html(htmlData);
+				// adding current page to top of history
+				history.pushState({}, "", `/${document.title}`);
+			});
 
-    $(document).on("click", ".contactMe", (event) => {
-      event.preventDefault();
-      $.get(`./views/contactme.html`, (htmlData) => {
-        $("main").html(htmlData);
-        history.pushState({}, "", `/${document.title}`);
-      });
-    });
+			LoadHome();
 
-    // redirecting user to aboutme page when the contace form is submitted
-    $(document).on("submit", "#formContact", function () {
-      event.preventDefault();
-      $.get(`./views/aboutme.html`, (htmlData) => {
-        $("main").html(htmlData);
-        history.pushState({}, "", `${document.title}`);
-      });
-    });
-  };
+			if(activePage = "Portfolio"){
+				$(document).on("click", ".contactMe", (event) =>{
+					event.preventDefault();
+					
+					$.get(`./views/contactme.html`, (activeLink) =>{
+						$("main").html(activeLink);
+						document.title= "Contact";
+						history.pushState({},"",`/${document.title}`);
+					});
+				});
+			};
+		
+			if(activePage = "Portfolio"){
+				$(document).on("click",".btnAboutme", (event) =>{
+					event.preventDefault();
 
-  let Start = () => {
-    LoadHeader();
-    LoadFooter();
+					$.get(`./views/aboutme.html`,(Aboutmelink) =>{
+						$("main").html(Aboutmelink);
+						document.title= "AboutMe";
+						history.pushState({},"", `/${document.title}`);
+					});
+				});
+			};
+		};
+        
+		
+	let LoadHome = () =>{
+		$.get(`./views/home.html`, (htmlData) => {
+			$("main").html(htmlData);
+			history.pushState({}, "", "/");
+		});	
+	};
+	let LoadAbout = () => {
+		$.get(`./views/aboutme.html`, (htmlData) => {
+		  $("main").html(htmlData);
+		  });
+	};
+	
+	let LoadContact = () => {
+		$.get(`./views/contactme.html`, (htmlData) => {
+		  $("main").html(htmlData);
+		  });
+	};
+	  
+	let LoadProjects = () => {
+	  $.get(`./views/projects.html`, (htmlData) => {
+		$("main").html(htmlData);
+		});
+	};
 
-    // setting the landing page to home
-    let activePage = document.title;
-    if (activePage == "Portfolio") {
-      document.title = "home";
-      // console.log("test here");
-      $.get(`./views/home.html`, (htmlData) => {
-        $("main").html(htmlData);
+	let LoadSkills = () =>{
+	  $.get(`./views/skills.html`, (htmlData)=>{
+		$("main").html(htmlData);
+		});
+	};
 
-        // adding home to top of history
-        history.pushState({}, "", `/${document.title}`);
-      });
-    }
-  };
+    let Start = () => {
 
-  window.addEventListener("load", Start);
+          LoadHeader();
+          LoadFooter();
+          LoadContent();
+		  
+
+
+        };
+          
+        
+
+          // setting the landing page to home
+          
+        //   if (document.title == "Portfolio") {
+        //     // console.log("test here");
+        //     $.get(`./views/home.html`, (htmlData) => {
+        //       $("main").html(htmlData);
+        //       console.log(document.title);
+        //       // adding home to top of history
+        //       history.pushState({}, "", `/${document.title}`);
+        //     });
+        //   }
+        
+        window.addEventListener("load", Start);
+	
 })();
